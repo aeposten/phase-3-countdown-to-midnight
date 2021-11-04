0cdf1587259c67a0994b536118a12e4300b7e891
Amy Posten <amyposten85@gmail.com>
2021-11-04 08:54:54 -0400

    commit tree c8edb3992b0da3945c1147aee1844a1b22773f7c

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
 #write your code here
 
-def countdown
+def countdown(num=10)
+    while num > 0
+        puts "#{num} SECOND(S)!"
+        num -=1
+    end
+    "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep (num=10)
+    while num > 0
+        puts "#{num} SECOND(S)!"
+        num -=1
+        sleep(1)
+    end
+    "HAPPY NEW YEAR!"
 end
